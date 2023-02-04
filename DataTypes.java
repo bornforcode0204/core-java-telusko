3ccedb9afa0a8bd3297cdefbc0d2fd699a9998b8
Shiv Singh <singhshiv0204@gmail.com>
2023-02-04 17:57:09 +0530

    initial commit

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte a = 8;
+        short b = 534;
+        int c = 7547;
+        long lvar = 53456456446l;
+
+        float price = 21.25f;
+        double d = 454.4545;
+
+        char ch = 'a';
+
+        boolean ispassed = false;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(ch);
+        System.out.println(price);
+        System.out.println(d);
+        System.out.println(lvar);
+        System.out.println(ispassed);
+    }
+}
---
+++ Literals.java
@@ -0,0 +1,14 @@
+public class Literals {
+    public static void main(String[] args) {
+        
+        int num1 = 34;
+
+        int num2 = 0b101;
+        int num3 = 0x7E;
+        int num4 = 10_00_00_000;
+        double num5 = 12e9; // 12 X 10^9
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
